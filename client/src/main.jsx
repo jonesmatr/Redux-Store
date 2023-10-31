0748b38942fca90bb7edc5e8c0066945c165a509
jonesmatr <32439889+jonesmatr@users.noreply.github.com>
2023-10-31 06:25:29 -0400

    Updated the main.jsx file

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,6 +2,9 @@ import ReactDOM from 'react-dom/client'
 import './index.css'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
+import { Provider } from 'react-redux';
+import store from './redux/store';
+
 import App from './App.jsx';
 import Home from './pages/Home';
 import Detail from './pages/Detail';
@@ -41,5 +44,7 @@ const router = createBrowserRouter([
 ]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <RouterProvider router={router} />
-)
+  <Provider store={store}>
+    <RouterProvider router={router} />
+  </Provider>
+);
