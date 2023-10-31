726583e9f83f1b023cfff526fbee5cd2354f6ec8
jonesmatr <32439889+jonesmatr@users.noreply.github.com>
2023-10-31 07:23:05 -0400

    Added environment variable

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,5 +1,9 @@
+require('dotenv').config(); // Load environment variables from .env file
+
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/mern-shopping');
+mongoose.connect(
+  process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/tech-thoughts'
+);
 
 module.exports = mongoose.connection;
